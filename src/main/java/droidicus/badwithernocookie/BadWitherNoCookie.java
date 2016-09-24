6d577ebb336b722f5b3046f1601299b28213ff7a
droidicus <droidicus@dr.com>
2016-09-24 01:09:09 -0400

    Adding Dragon silence and make code client only

--- src/main/java/droidicus/badwithernocookie/BadWitherNoCookie.java
+++ src/main/java/droidicus/badwithernocookie/BadWitherNoCookie.java
@@ -8,6 +8,8 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import net.minecraft.init.SoundEvents;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod(modid = BadWitherNoCookie.MODID, version = BadWitherNoCookie.VERSION)
 public class BadWitherNoCookie  {
@@ -15,6 +17,7 @@ public class BadWitherNoCookie  {
     public static final String VERSION = "0.0.1";
     
     @EventHandler
+    @SideOnly(Side.CLIENT)
     public void init(FMLInitializationEvent event) {
         // some example code
         System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
@@ -33,6 +36,22 @@ public class BadWitherNoCookie  {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
             System.out.println("IllegalAccessException, Wither sound not silenced :-(");
+            e.printStackTrace();
+        }
+
+        try {
+            Field field = SoundEvents.class.getField("ENTITY_ENDERDRAGON_DEATH");
+            field.setAccessible(true);
+
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+            field.set(null, null);
+        } catch (NoSuchFieldException e) {
+            System.out.println("NoSuchFieldException, Dragon sound not silenced :-(");
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            System.out.println("IllegalAccessException, Dragon sound not silenced :-(");
             e.printStackTrace();
         }
 
