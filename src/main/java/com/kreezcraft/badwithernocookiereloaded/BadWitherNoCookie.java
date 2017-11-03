1109c8a1145a03e9d68a22f4c7efcdd9467e31ac
Kreezxil <kreezxil@gmail.com>
2017-11-03 18:37:12 -0500

    added toggle command /listen
    that display sound event names in chat to the player.
    these names are to be used in the config file to silence sounds from any
    mod.

--- src/main/java/com/kreezcraft/badwithernocookiereloaded/BadWitherNoCookie.java
+++ src/main/java/com/kreezcraft/badwithernocookiereloaded/BadWitherNoCookie.java
@@ -1,5 +1,7 @@
 package com.kreezcraft.badwithernocookiereloaded;
 
+import net.minecraft.command.ICommand;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
@@ -7,22 +9,31 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.io.File;
 
+import org.apache.logging.log4j.Logger;
+
 @Mod(modid = BadWitherNoCookie.MODID, name = BadWitherNoCookie.NAME, version = BadWitherNoCookie.VERSION)
 public class BadWitherNoCookie {
     public static final String MODID = "badwithernocookiereloaded";
     public static final String NAME = "Bad Wither No Cookie! Reloaded";
-    public static final String VERSION = "1.9.4-1.9";
+    public static final String VERSION = "1.9.4-2.12";
     
     // Config instance
     public static Configuration config;
+    
+    public static Logger logger;
+    
+    public static boolean whatWasThat = false;
+    public static EntityPlayer player;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent e) {
+    	logger = e.getModLog();
         // read from config file
         File directory = e.getModConfigurationDirectory();
         config = new Configuration(new File(directory.getPath(), "badwithernocookie.cfg"));
@@ -44,4 +55,8 @@ public class BadWitherNoCookie {
         }
     }
 
+    @Mod.EventHandler
+    public void serverLoad(FMLServerStartingEvent event) {
+    	event.registerServerCommand((ICommand) new ListenCommand());
+    }
 }
--- src/main/java/com/kreezcraft/badwithernocookiereloaded/Config.java
+++ src/main/java/com/kreezcraft/badwithernocookiereloaded/Config.java
@@ -8,12 +8,14 @@ import net.minecraftforge.common.config.Configuration;
 public class Config {
 
     private static final String CATEGORY_GENERAL = "general";
+    private static final String CATEGORY_EXTRA = "extra sounds to silence";
 
     // This values below you can access elsewhere in your mod:
     public static boolean silenceWither = true;
     public static boolean silenceDragon = true;
     public static boolean silenceLightning = true;
     public static boolean silenceSuccess = true;
+    public static String[] silenceUs;
 
     // Call this from CommonProxy.preInit(). It will create our config if it doesn't
     // exist yet and read the values if it does exist.
@@ -39,5 +41,7 @@ public class Config {
         silenceDragon = cfg.getBoolean("silenceDragon", CATEGORY_GENERAL, silenceDragon, "Set to true to silence the server-wide Ender Dragon death broadcast sound.");
         silenceLightning = cfg.getBoolean("silenceLightning", CATEGORY_GENERAL, silenceLightning, "Set to true to silence the server-wide Thunder caused by Lightning broadcast sound.");
         silenceSuccess = cfg.getBoolean("silenceSuccess", CATEGORY_GENERAL, silenceSuccess, "Set to true to silence the server messages for when any of these sounds have been silenced.");
+        cfg.addCustomCategoryComment(CATEGORY_EXTRA, "The sounds of silence");
+        silenceUs = cfg.getStringList("silenceUs", CATEGORY_EXTRA, new String[] {""}, "A list of sounds to silence, discoverable with the toggle command /listen\nenter one sound event per line with no commas");
     }
 }
